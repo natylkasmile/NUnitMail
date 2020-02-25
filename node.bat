3e12b3287f18ae24257d3b922e17a5793e39a37f
natylkasmile <natylkasmile@gmail.com>
2020-02-25 11:20:38 +0400

    Update node.bat

--- node.bat
+++ node.bat
@@ -1,1 +1,1 @@
-java -Dwebdriver.chrome.driver="%CD%\chromedriver.exe" -jar selenium-server-standalone-3.141.59.jar -role node -hub http://192.168.88.237:4444/grid/register/  -port 14572
+java -Dwebdriver.chrome.driver="%CD%\chromedriver.exe" -jar selenium-server-standalone-3.141.59.jar -role node -hub http://localhost:4444/grid/register/  -port 14572
